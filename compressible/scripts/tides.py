567219b2f4581865d983d8fc3d548d8cf5382dc4
Keaton J. Burns <keaton.burns@gmail.com>
2018-01-18 23:21:17 -0500

    Add eigenmode problems

--- compressible/scripts/tides.py
+++ compressible/scripts/tides.py
@@ -11,8 +11,8 @@ logger = logging.getLogger(__name__)
 def linear_tide_1d(param, comm=MPI.COMM_SELF):
     """Solve linear tide with k=k_tide in 1D."""
     # Background BVP
-    domain, p_full = atmos.solve_hydrostatic_pressure(param, np.complex128, comm=comm)
-    p_full, p_trunc, a_full, a_trunc, heq, N2 = atmos.truncate_background(param, p_full)
+    domain, p_full, a_full = atmos.solve_hydrostatic_pressure(param, np.complex128, comm=comm)
+    p_full, p_trunc, a_full, a_trunc, heq, N2 = atmos.truncate_background(param, p_full, a_full)
     # Adiabatic viscous fully-compressible hydrodynamics
     problem = de.LBVP(domain, variables=['a1','p1','u','w','uz','wz'],
         ncc_cutoff=param.ivp_cutoff, entry_cutoff=param.matrix_cutoff)
@@ -57,8 +57,8 @@ def linear_tide_1d(param, comm=MPI.COMM_SELF):
 def linear_tide_2d(param, comm=MPI.COMM_WORLD):
     """Solve linear tide in 2D."""
     # Solve background
-    domain_bvp, p_full = atmos.solve_hydrostatic_pressure(param, np.float64)
-    p_full, p_trunc, a_full, a_trunc, heq, N2 = atmos.truncate_background(param, p_full)
+    domain_bvp, p_full, a_full = atmos.solve_hydrostatic_pressure(param, np.float64)
+    p_full, p_trunc, a_full, a_trunc, heq, N2 = atmos.truncate_background(param, p_full, a_full)
     # IVP domain
     x_basis = de.Fourier('x', param.Nx, interval=(0, param.Lx), dealias=2)
     z_basis = de.Chebyshev('z', param.Nz, interval=(0, param.Lz), dealias=2)
@@ -122,3 +122,87 @@ def linear_tide_2d(param, comm=MPI.COMM_WORLD):
     return domain, problem
 
 
+def eigenmodes_1d(param, kx, comm=MPI.COMM_SELF):
+    """Solve normal modes for any kx in 1D."""
+    # Background BVP
+    domain, p_full, a_full = atmos.solve_hydrostatic_pressure(param, np.complex128, comm=comm)
+    p_full, p_trunc, a_full, a_trunc, heq, N2 = atmos.truncate_background(param, p_full, a_full)
+    # Adiabatic viscous fully-compressible hydrodynamics
+    problem = de.EVP(domain, variables=['a1','p1','u','w','uz','wz'], eigenvalue='σ',
+        ncc_cutoff=param.ivp_cutoff, entry_cutoff=param.matrix_cutoff)
+    problem.meta[:]['z']['dirichlet'] = True
+    problem.parameters['a0'] = a_trunc
+    problem.parameters['p0'] = p_trunc
+    problem.parameters['a0z'] = a_trunc.differentiate('z')
+    problem.parameters['p0z'] = p_trunc.differentiate('z')
+    problem.substitutions['a0x'] = '0'
+    problem.substitutions['p0x'] = '0'
+    problem.parameters['U'] = 0  # Look at modes in local frame
+    problem.parameters['μ'] = param.μ
+    problem.parameters['γ'] = param.γ
+    problem.parameters['kx'] = kx  # Instead of k_tide
+    problem.parameters['Lz'] = param.Lz
+    problem.substitutions['dt(A)'] = "-1j*σ*A"
+    problem.substitutions['dx(A)'] = "1j*kx*A"
+    problem.substitutions['ux'] = "dx(u)"
+    problem.substitutions['wx'] = "dx(w)"
+    problem.substitutions['div_u'] = "ux + wz"
+    problem.substitutions['txx'] = "μ*(2*ux - 2/3*div_u)"
+    problem.substitutions['txz'] = "μ*(wx + uz)"
+    problem.substitutions['tzz'] = "μ*(2*wz - 2/3*div_u)"
+    problem.substitutions['φ'] = "0"
+    problem.substitutions['cs20'] = "γ*p0*a0"
+    problem.add_equation("dt(u) + U*ux + a0*dx(p1) + a1*p0x - a0*(dx(txx) + dz(txz)) = - (u*ux + w*uz) - a1*dx(p1) + a1*(dx(txx) + dz(txz)) - dx(φ)")
+    problem.add_equation("dt(w) + U*wx + a0*dz(p1) + a1*p0z - a0*(dx(txz) + dz(tzz)) = - (u*wx + w*wz) - a1*dz(p1) + a1*(dx(txz) + dz(tzz)) - dz(φ)")
+    problem.add_equation("dt(a1) + U*dx(a1) + u*a0x + w*a0z -   a0*div_u = - (U*a0x + u*dx(a1) + w*dz(a1)) +   a1*div_u")
+    problem.add_equation("dt(p1) + U*dx(p1) + u*p0x + w*p0z + γ*p0*div_u = - (U*p0x + u*dx(p1) + w*dz(p1)) - γ*p1*div_u")
+    problem.add_equation("uz - dz(u) = 0")
+    problem.add_equation("wz - dz(w) = 0")
+    problem.add_bc("left(txz/μ) = 0")
+    problem.add_bc("right(txz/μ) = 0")
+    problem.add_bc("left(w) = 0")
+    problem.add_bc("right(w) = 0")
+    return domain, problem
+
+def eigenmodes_inviscid_1d(param, kx, comm=MPI.COMM_SELF):
+    """Solve normal modes for any kx in 1D."""
+    # Background BVP
+    domain, p_full, a_full = atmos.solve_hydrostatic_pressure(param, np.complex128, comm=comm)
+    p_full, p_trunc, a_full, a_trunc, heq, N2 = atmos.truncate_background(param, p_full, a_full)
+    # Adiabatic viscous fully-compressible hydrodynamics
+    problem = de.EVP(domain, variables=['a1','p1','u','w','uz','wz'], eigenvalue='σ',
+        ncc_cutoff=param.ivp_cutoff, entry_cutoff=param.matrix_cutoff)
+    problem.meta[:]['z']['dirichlet'] = True
+    problem.parameters['a0'] = a_trunc
+    problem.parameters['p0'] = p_trunc
+    problem.parameters['a0z'] = a_trunc.differentiate('z')
+    problem.parameters['p0z'] = p_trunc.differentiate('z')
+    problem.substitutions['a0x'] = '0'
+    problem.substitutions['p0x'] = '0'
+    problem.parameters['U'] = 0  # Look at modes in local frame
+    problem.substitutions['μ'] = '0'  # Solve for inviscid modes
+    problem.parameters['γ'] = param.γ
+    problem.parameters['kx'] = kx  # Instead of k_tide
+    problem.parameters['Lz'] = param.Lz
+    problem.substitutions['dt(A)'] = "-1j*σ*A"
+    problem.substitutions['dx(A)'] = "1j*kx*A"
+    problem.substitutions['ux'] = "dx(u)"
+    problem.substitutions['wx'] = "dx(w)"
+    problem.substitutions['div_u'] = "ux + wz"
+    problem.substitutions['txx'] = "μ*(2*ux - 2/3*div_u)"
+    problem.substitutions['txz'] = "μ*(wx + uz)"
+    problem.substitutions['tzz'] = "μ*(2*wz - 2/3*div_u)"
+    problem.substitutions['φ'] = "0"
+    problem.substitutions['cs20'] = "γ*p0*a0"
+    problem.add_equation("dt(u) + U*ux + a0*dx(p1) + a1*p0x - a0*(dx(txx) + dz(txz)) = - (u*ux + w*uz) - a1*dx(p1) + a1*(dx(txx) + dz(txz)) - dx(φ)")
+    problem.add_equation("dt(w) + U*wx + a0*dz(p1) + a1*p0z - a0*(dx(txz) + dz(tzz)) = - (u*wx + w*wz) - a1*dz(p1) + a1*(dx(txz) + dz(tzz)) - dz(φ)")
+    problem.add_equation("dt(a1) + U*dx(a1) + u*a0x + w*a0z -   a0*div_u = - (U*a0x + u*dx(a1) + w*dz(a1)) +   a1*div_u")
+    problem.add_equation("dt(p1) + U*dx(p1) + u*p0x + w*p0z + γ*p0*div_u = - (U*p0x + u*dx(p1) + w*dz(p1)) - γ*p1*div_u")
+    problem.add_equation("uz - dz(u) = 0")
+    problem.add_equation("wz - dz(w) = 0")
+    #problem.add_bc("left(txz) = 0")
+    #problem.add_bc("right(txz) = 0")
+    problem.add_bc("left(uz - dz(u)) = 0")
+    problem.add_bc("left(w) = 0")
+    problem.add_bc("right(w) = 0")
+    return domain, problem
